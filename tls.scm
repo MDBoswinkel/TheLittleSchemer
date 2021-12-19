be5c375fbcaff85c3f9da004c13af36d65815dd4
MDBoswinkel <mdboswinkel@outlook.com>
2021-12-19 19:31:56 +0100

    Third commit

--- tls.scm
+++ tls.scm
@@ -282,3 +282,29 @@
   (lambda (lat)
     (cond ((null? lat) (quote()))
           (else (cons (car lat) (makeset (multirember (car lat) (cdr lat))))))))
+
+(define subset?
+  (lambda (set1 set2)
+    (cond ((null? set1) #t)
+          (else (and (member? (car set1) set2) (subset? (cdr set1) set2))))))
+
+(define eqset?
+  (lambda (set1 set2)
+    (and (subset? set1 set2) (subset? set2 set1))))
+
+(define intersect?
+  (lambda (set1 set2)
+    (cond ((null? set1) #f)
+          (else (or (member? (car set1) set2) (intersect? (cdr set1) set2))))))
+
+(define intersect
+  (lambda (set1 set2)
+    (cond ((null? set1) (quote()))
+          ((member? (car set1) set2) (cons (car set1) (intersect (cdr set1) set2)))
+          (else (intersect (cdr set1) set2)))))
+
+(define union
+  (lambda (set1 set2)
+    (cond ((null? set1) set2)
+          ((member? (car set1) set2) (union (cdr set1) set2))
+          (else (cons (car set1) (union (cdr set1) set2))))))
